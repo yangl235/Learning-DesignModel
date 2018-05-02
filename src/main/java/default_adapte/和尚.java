5bd505259f0d28818c0da67ea813cf4da7547f96
yanglei18 <yanglei18@meituan.com>
2018-05-02 19:30:15 +0800

    缺省适配模式

---
+++ src/main/java/default_adapte/和尚.java
@@ -0,0 +1,15 @@
+package default_adapte;
+
+public interface 和尚 {
+    public void 吃斋();
+
+    public void 念经();
+
+    public void 打坐();
+
+    public void 撞钟();
+
+    public void 习武();
+
+    public String getName();
+}
---
+++ src/main/java/default_adapte/鲁智深.java
@@ -0,0 +1,27 @@
+package default_adapte;
+
+public class 鲁智深 implements 和尚 {
+    public void 吃斋() {
+
+    }
+
+    public void 念经() {
+
+    }
+
+    public void 打坐() {
+
+    }
+
+    public void 撞钟() {
+
+    }
+
+    public void 习武() {
+
+    }
+
+    public String getName() {
+        return "鲁智深";
+    }
+}
