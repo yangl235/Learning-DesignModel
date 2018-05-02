2af4386fe848ef366c4b40d69d33c59864d1ea10
yanglei18 <yanglei18@meituan.com>
2018-05-02 14:53:20 +0800

    类级别的适配器模式

---
+++ src/main/java/adapter/class_level/Adaptee.java
@@ -0,0 +1,9 @@
+package adapter.class_level;
+
+public class Adaptee {
+    /**
+     * 源类含有方法sampleOperation1()
+     */
+    public void sampleOperation1() {
+    }
+}
---
+++ src/main/java/adapter/class_level/Adapter.java
@@ -0,0 +1,10 @@
+package adapter.class_level;
+
+public class Adapter extends Adaptee implements Target {
+    /**
+     * 源类没有方法sampleOperation2()，适配器类补充上这个方法
+     */
+    public void sampleOperation2() {
+        //your code
+    }
+}
